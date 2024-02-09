27ca469065a405e515d13ac8d541ef8feb200103
Dean Cohen <Dean5102000@gmail.com>
2024-02-09 16:23:30 +0200

    Added functionality to 'Post' button and to 'Delete' button

--- src/MiddleSection/MidComponents/Post.js
+++ src/MiddleSection/MidComponents/Post.js
@@ -2,18 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/MidSection/Post.css'; // Import the Post component CSS file
 
-function Post({ username, userPic, postText, postImage, postTime }) {
+function Post({ id, username, userPic, postText, postImage, postTime, onDelete }) {
+  const handleDelete = () => {
+    onDelete(id); // Call the onDelete function with the post ID as an argument
+  };
+
+  const handleEdit = () => {
+    // Implement the edit functionality as needed
+    console.log(`Editing post with ID: ${id}`);
+  };
+
   return (
     <div className="post">
       <div className="post-header">
         <div className="user-info">
+          {/* The user pic needs to be modified to show the real user pic */}
           <img src={userPic} alt="Profile Pic" className="profile-pic" />
           <div className="username">{username}</div>
           <div className="post-time">{postTime}</div> {/* Display post time */}
         </div>
         <div className="post-actions">
-          <button className="delete-post-btn">Delete Post</button>
-          <button className="edit-post-btn">Edit Post</button>
+          <button className="delete-post-btn" onClick={handleDelete}>Delete Post</button>
+          <button className="edit-post-btn" onClick={handleEdit}>Edit Post</button>
         </div>
       </div>
       <div className="post-content">
@@ -30,11 +40,56 @@ function Post({ username, userPic, postText, postImage, postTime }) {
 }
 
 Post.propTypes = {
+  id: PropTypes.number.isRequired, // Add the id prop type
   username: PropTypes.string.isRequired,
-  profilePic: PropTypes.string.isRequired,
+  profilePic: PropTypes.string.isRequired, // Corrected prop name to profilePic
   postText: PropTypes.string.isRequired,
   postImage: PropTypes.string, // Post image is optional
   postTime: PropTypes.string.isRequired, // Time of the post
+  onDelete: PropTypes.func.isRequired, // onDelete function to handle post deletion
 };
 
 export default Post;
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import '../../styles/MidSection/Post.css'; // Import the Post component CSS file
+
+// function Post({ username, userPic, postText, postImage, postTime }) {
+
+//   return (
+//     <div className="post">
+//       <div className="post-header">
+//         <div className="user-info">
+//           {/* The user pic needs to be modify to show the real user pic */}
+//           <img src={userPic} alt="Profile Pic" className="profile-pic" />
+//           <div className="username">{username}</div>
+//           <div className="post-time">{postTime}</div> {/* Display post time */}
+//         </div>
+//         <div className="post-actions">
+//           <button className="delete-post-btn">Delete Post</button>
+//           <button className="edit-post-btn">Edit Post</button>
+//         </div>
+//       </div>
+//       <div className="post-content">
+//         <p className="post-text">{postText}</p>
+//         {postImage && <img src={postImage} alt="Post" className="post-image" />} {/* Display post image if available */}
+//       </div>
+//       <div className="post-footer">
+//         <button className="like-btn">Like</button>
+//         <button className="comment-btn">Comment</button>
+//         <button className="share-btn">Share</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// Post.propTypes = {
+//   username: PropTypes.string.isRequired,
+//   profilePic: PropTypes.string.isRequired,
+//   postText: PropTypes.string.isRequired,
+//   postImage: PropTypes.string, // Post image is optional
+//   postTime: PropTypes.string.isRequired, // Time of the post
+// };
+
+// export default Post;

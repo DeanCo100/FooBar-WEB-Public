5ab27fdb738bd5d4cf585592f910639810dc90e7
Dean Cohen <Dean5102000@gmail.com>
2024-02-14 20:42:50 +0200

    Created a path to login page and blocked the access to the feed.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter and Routes
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/login/login';
 import Feed from '../src/Feed/Feed';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode(prevMode => !prevMode);
+  };
+
+  const handleLogin = (username, password) => {
+    // Hardcoded username and password for demonstration purposes
+    const hardcodedUsername = 'TzionMea';
+    const hardcodedPassword = 'Mea100100';
+
+    if (username === hardcodedUsername && password === hardcodedPassword) {
+      setIsAuthenticated(true);
+    } else {
+      // Show error message or handle invalid login
+      console.log("Invalid username or password");
+    }
   };
 
   return (
     <Router>
       <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
         <Routes>
-          <Route path="/" element={<Feed darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} />
-          <Route path="/login" element={<LoginPage />} />
+          {/* Instead of Feed in the first route it should be loginPage */}
+          <Route path="/login" element={<Feed onLogin={handleLogin} />} />
+          <Route path="/" element={isAuthenticated ? <Feed darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> : <Navigate to="/login" />} />
         </Routes>
       </div>
     </Router>
